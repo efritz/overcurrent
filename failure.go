6f2275c1294cf5f0e3de8b169165ad92b95f4cad
Eric Fritz <eric@eric-fritz.com>
2016-04-24 10:51:43 -0500

    Add documentation.

--- failure.go
+++ failure.go
@@ -1,5 +1,9 @@
 package overcurrent
 
+// FailureInterpreter is the interface that determines if an error should
+// affect whether or not the circuit breaker will trip. This is useful if
+// some errors are indictive of a system failure and others are not (e.g.
+// HTTP 500 vs HTTP 400 responses).
 type FailureInterpreter interface {
 	ShouldTrip(error) bool
 }
@@ -13,6 +17,7 @@ func (fi *AnyErrorFailureInterpreter) ShouldTrip(err error) bool {
 	return true
 }
 
+// A failure interpreter that trips on every error.
 func NewAnyErrorFailureInterpreter() *AnyErrorFailureInterpreter {
 	return &AnyErrorFailureInterpreter{}
 }
--- trip.go
+++ trip.go
@@ -1,8 +1,21 @@
 package overcurrent
 
+// TripCondition is the interface that controls the trip state of the circuit
+// breaker.
 type TripCondition interface {
+	// Invoked when the circuit breaker was closed or half-closed and failure
+	// occurs, or when the breaker is hard-tripped. Stats should be collected
+	// at this point.
 	Failure()
+
+	// Invoked when the a call to a service completes successfully (note - all
+	// successful calls, not just ones immediately after a failure period), or
+	// when the breaker is hard reset. Stats should be reset at this point.
 	Success()
+
+	// Determines the state of the breaker - returns true or false for open and
+	// closed state, respectively. This method should be idempotent and use the
+	// state altered by the failure and success methods.
 	ShouldTrip() bool
 }
 
@@ -18,6 +31,7 @@ func (tc *ConsecutiveFailureTripCondition) Failure()         { tc.count++ }
 func (tc *ConsecutiveFailureTripCondition) Success()         { tc.count = 0 }
 func (tc *ConsecutiveFailureTripCondition) ShouldTrip() bool { return tc.count >= tc.threshold }
 
+// A trip condition that trips the circuit breaker after `threshold` failures in a row.
 func NewConsecutiveFailureTripCondition(threshold int) *ConsecutiveFailureTripCondition {
 	return &ConsecutiveFailureTripCondition{
 		count:     0,
