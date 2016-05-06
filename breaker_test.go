37715fb2e54f9c8e2f8d731cb6a8e4d673165071
Eric Fritz <eric@eric-fritz.com>
2016-05-05 21:59:44 -0500

    Add test for additional coverage.

--- breaker_test.go
+++ breaker_test.go
@@ -66,6 +66,23 @@ func (s *OvercurrentSuite) TestTimeoutTrip(c *C) {
 	c.Assert(cb.Call(fn), Equals, CircuitOpenError)
 }
 
+func (s *OvercurrentSuite) TimeoutDisabled(c *C) {
+	cb := NewBreaker(BreakerConfig{
+		InvocationTimeout:          0,
+		ResetTimeout:               250 * time.Millisecond,
+		HalfClosedRetryProbability: 1,
+		FailureInterpreter:         NewAnyErrorFailureInterpreter(),
+		TripCondition:              NewConsecutiveFailureTripCondition(5),
+	})
+
+	fn := func() error {
+		<-time.After(250 * time.Millisecond)
+		return nil
+	}
+
+	c.Assert(cb.Call(fn), Equals, nil)
+}
+
 func (s *OvercurrentSuite) TestHalfOpenFailure(c *C) {
 	cb := NewBreaker(BreakerConfig{
 		InvocationTimeout:          DefaultInvocationTimeout,
