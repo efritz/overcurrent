a9469c054f73103a39f54bd66ba75306ccf773c8
Eric Fritz <eric@eric-fritz.com>
2016-06-09 21:51:29 -0500

    Small code reorder.

--- config.go
+++ config.go
@@ -11,29 +11,26 @@ const (
 	DefaultHalfClosedRetryProbability = .5
 )
 
-var (
-	DefaultResetBackOff = backoff.NewConstantBackOff(1000 * time.Millisecond)
-)
-
 //
 //
 
 type CircuitBreakerConfig struct {
 	InvocationTimeout          time.Duration
-	ResetBackOff               BackOff
 	HalfClosedRetryProbability float64
+	ResetBackOff               BackOff
 	FailureInterpreter         FailureInterpreter
 	TripCondition              TripCondition
 }
 
 // Creates a circuit breaker config usign the default values for timeouts
-// and half-closed retry probability, an any-error failure interpreter, and
-// a consecutive-failure trip condition (with a value of five).
+// and half-closed retry probability, a constnat retry backoff of 1000ms,
+// an any-error failure interpreter, and atrip condition which fires only
+// after five ocnsecutive failures.
 func DefaultCircuitBreakerConfig() *CircuitBreakerConfig {
 	return &CircuitBreakerConfig{
 		InvocationTimeout:          DefaultInvocationTimeout,
-		ResetBackOff:               DefaultResetBackOff,
 		HalfClosedRetryProbability: DefaultHalfClosedRetryProbability,
+		ResetBackOff:               backoff.NewConstantBackOff(1000 * time.Millisecond),
 		FailureInterpreter:         NewAnyErrorFailureInterpreter(),
 		TripCondition:              NewConsecutiveFailureTripCondition(5),
 	}
